0508e41d5b62ff2d18ea5c248a8da3c245de1b9c
Melisha <melisha.evans06@gmail.com>
2023-09-02 18:04:39 -0700

    updated index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,15 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category)
 // Categories have many Products
-
+Categories.haveMany(Product)
 // Products belongToMany Tags (through ProductTag)
-
+Products.belongToMany(Tag)
 // Tags belongToMany Products (through ProductTag)
+Tage.belongToMany(Product)
+
+
 
 module.exports = {
   Product,
